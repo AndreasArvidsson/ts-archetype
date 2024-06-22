6aacc7c6606143254f59e75631430bea10f0b1af
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-06-22 16:00:46 +0200

    fixed typo

--- src/updatePrettierignore.ts
+++ src/updatePrettierignore.ts
@@ -3,7 +3,7 @@ import type { Config } from "./types";
 export const updatePrettierignore = (config: Config) => {
     const lines = ["node_modules", ...(config.react ? [] : ["/lib"]), "package-lock.json"];
 
-    const defaultGitIgnore = lines.join("\n") + "\n";
+    const defaultPrettierIgnore = lines.join("\n") + "\n";
 
-    return (actual: string | null) => actual || defaultGitIgnore;
+    return (actual: string | null) => actual || defaultPrettierIgnore;
 };
